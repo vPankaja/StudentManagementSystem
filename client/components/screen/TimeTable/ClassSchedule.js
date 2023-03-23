22e747df22fd462e6ec5afcf970cbc9f4c1b289c
vPankaja <vpankaja42@gmail.com>
2023-03-23 23:53:22 +0530

    second commit

--- client/components/screen/TimeTable/ClassSchedule.js
+++ client/components/screen/TimeTable/ClassSchedule.js
@@ -6,7 +6,8 @@ import {
   Button,
   StyleSheet,
   ToastAndroid,
-  TouchableOpacity,
+  TimePickerAndroid,
+  Picker,
 } from "react-native";
 import { db } from "../../firebase-config/firebase-config";
 import { useNavigation } from "@react-navigation/native";
@@ -16,10 +17,46 @@ export default function AddClassSchedule() {
   const [data, setData] = useState("");
   const navigation = useNavigation();
   const DatCollectinRef = collection(db, "Class Schedule"); //database collection reference
+  const [selectedDay, setSelectedDay] = useState("");
 
   //inputs handle function
-  const handleChangeText = (name, value) => {
-    setData((prevState) => ({ ...prevState, [name]: value }));
+  const handleChangeText = async (name, value) => {
+    if (name === "subject" && value.trim() === "") {
+      // If the subject field is empty, show an error message
+      ToastAndroid.show("Please enter a subject.", ToastAndroid.SHORT);
+      return;
+    }
+
+    // Validate the selectedDay value
+    if (name === "day" && value === "") {
+      // If the day is not selected, show an error message
+      ToastAndroid.show("Please select a day.", ToastAndroid.SHORT);
+      return;
+    }
+
+    if (name === "time") {
+      // If the Time input field is clicked, show the time picker
+      try {
+        const { action, hour, minute } = await TimePickerAndroid.open({
+          hour: 12,
+          minute: 0,
+          is24Hour: false,
+        });
+        if (action !== TimePickerAndroid.dismissedAction) {
+          // Set the selected time to the Time input field
+          let selectedTime = `${hour}:${minute}`;
+          setData((prevState) => ({ ...prevState, [name]: selectedTime }));
+        }
+      } catch (error) {
+        console.warn("Cannot open time picker", error);
+      }
+    } else {
+      setData((prevState) => ({
+        ...prevState,
+        [name]: value,
+        day: selectedDay,
+      }));
+    }
   };
 
   //create user function,include firebase methods
@@ -28,8 +65,10 @@ export default function AddClassSchedule() {
       await addDoc(DatCollectinRef, {
         day: data.day,
         time: data.time,
+        duration: data.duration,
         venue: data.venue,
         subject: data.subject,
+        lecturer: data.lecturer,
       });
       if (addDoc) {
         ToastAndroid.show(
@@ -57,32 +96,57 @@ export default function AddClassSchedule() {
           padding: 10,
         }}
       >
-        <Text style={styles.text}>Date</Text>
-        <TextInput
+        <Text style={styles.text}>Day</Text>
+        <Picker
           style={styles.input}
-          placeholder="Enter Date"
-          onChangeText={(val) => handleChangeText("day", val)}
-        />
+          selectedValue={selectedDay}
+          onValueChange={(itemValue, itemIndex) => setSelectedDay(itemValue)}
+        >
+          <Picker.Item label="Select a Day" value="" />
+          <Picker.Item label="Monday" value="Monday" />
+          <Picker.Item label="Tuesday" value="Tuesday" />
+          <Picker.Item label="Wednesday" value="Wednesday" />
+          <Picker.Item label="Thursday" value="Thursday" />
+          <Picker.Item label="Friday" value="Friday" />
+          <Picker.Item label="Saturday" value="Saturday" />
+          <Picker.Item label="Sunday" value="Sunday" />
+        </Picker>
 
         <Text style={styles.text}>Time</Text>
         <TextInput
           style={styles.input}
-          placeholder="Enter Time"
-          onChangeText={(val) => handleChangeText("time", val)}
+          placeholder="Select the Time"
+          editable={false}
+          value={data.time}
+          onTouchStart={() => handleChangeText("time")}
+        />
+
+        <Text style={styles.text}>Duration</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter the Duration"
+          onChangeText={(val) => handleChangeText("duration", val)}
         />
 
         <Text style={styles.text}>Venue</Text>
         <TextInput
           style={styles.input}
-          placeholder="Enter Venue"
+          placeholder="Enter the Venue"
           onChangeText={(val) => handleChangeText("venue", val)}
         />
 
         <Text style={styles.text}>Subject</Text>
         <TextInput
           style={styles.input}
-          placeholder="Subject"
+          placeholder="Enter the Subject"
           onChangeText={(val) => handleChangeText("subject", val)}
+        />
+
+        <Text style={styles.text}>Lecturer</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter the lecturer's name"
+          onChangeText={(val) => handleChangeText("lecturer", val)}
         />
         <br />
         <Button title="Add Class" onPress={() => add_data()} />
